97569b9b931ac41f4951833d3383b4d0ee12ecba
ryj <1614912120@qq.com>
2025-06-20 00:26:30 +0800

    11

---
+++ src/main/java/com/quanxiaoha/biz/operationlog/sdsdasd/SdsdasdApplication.java
@@ -0,0 +1,13 @@
+package com.quanxiaoha.biz.operationlog.sdsdasd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdsdasdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdsdasdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/quanxiaoha/biz/operationlog/sdsdasd/SdsdasdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quanxiaoha.biz.operationlog.sdsdasd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdsdasdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
